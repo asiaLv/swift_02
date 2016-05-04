527f97ff976e45c50fd9bf1f79448b2378abf0fd
lyz <18539272718@163.com>
2016-05-04 10:36:52 +0800

    Initial Commit

---
+++ swift_day02Tests/swift_day02Tests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_day02Tests.swift
+//  swift_day02Tests
+//
+//  Created by lvAsia on 16/5/4.
+//  Copyright © 2016年 answe lv. All rights reserved.
+//
+
+import XCTest
+@testable import swift_day02
+
+class swift_day02Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
